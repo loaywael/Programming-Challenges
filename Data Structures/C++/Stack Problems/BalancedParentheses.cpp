7bce095d0ca06a4cc0ae672c329cfb2108e123b0
loaywael <hythamconn1@gmail.com>
2019-11-18 00:52:49 +0200

    Stack Problems

--- Data Structures/C++/Stack Problems/BalancedParentheses.cpp
+++ Data Structures/C++/Stack Problems/BalancedParentheses.cpp
@@ -3,13 +3,34 @@
 #include <stack>
 
 bool isBalanced(const std::string& exp); 
+bool isPair(const char& ch1, const char& ch2);
 
 int main(void){
 
-    std::string sF = "[{]djjf";
-    std::string sT = "[{djjf}]";
-    std::cout << isBalanced(sF) << std::endl;
-    std::cout << isBalanced(sT) << std::endl;
+    std::string s;
+    while (1){
+        std::cout << "Enter string: ";
+        getline(std::cin, s);
+        if (s == "q"){break;}
+        std::string state = isBalanced(s)? "Balanced" : "Not Balanced";
+        std::cout <<  state << std::endl; 
+        
+    }   
+    return 0;
+}
+
+bool isPair(const char& ch1, const char& ch2){
+    if (ch1 == '{' && ch2 == '}'){
+        return 1;
+    }
+    else if (ch1 == '[' && ch2 == ']'){
+        return 1;
+    }
+
+    if (ch1 == '(' && ch2 == ')'){
+        return 1;
+    }
+
     return 0;
 }
 
@@ -17,10 +38,13 @@ int main(void){
 bool isBalanced(const std::string& exp){
     std::stack<char> buffer;
     for(char ch : exp){
-        if(ch == '[' || ch =='{'){
+        if (ch == '[' || ch =='{' || ch == '('){
             buffer.push(ch);
         }
-        else if(ch == ']' || ch == '}'){
+        else if (ch == ']' || ch == '}' || ch == ')'){
+            if (buffer.empty() || !isPair(buffer.top(), ch)){
+                return 0;
+            }
             buffer.pop();
         }
     }
